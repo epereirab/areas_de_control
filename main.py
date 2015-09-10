ecb581206ce4fc040412b8546a039b81ff00f156
Felipe <fgonzalezvenegas@gmail.com>
2015-09-10 15:15:42 -0300

    Minor changes for readibility.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-__author__ = 'MPro'
-
 from coopr.pyomo import *
 #from pyomo.environ import *
 from coopr.opt import SolverFactory
@@ -10,12 +8,13 @@ import csv
 import cStringIO
 import sys
 
+__author__ = 'MPro'
 ####  - - - - - - LEER RUTAS DE DATOS Y RESULTADOS  - - - - - - #######
 
 config_rutas = open('config_rutas.txt', 'r')
-path_datos=''
-path_resultados=''
-tmp_line=''
+path_datos = ''
+path_resultados = ''
+tmp_line = ''
 
 for line in config_rutas:
 
@@ -27,17 +26,23 @@ for line in config_rutas:
     tmp_line = line.split()[0]
 
 ####  - - - - - - CARGAR DATOS AL MODELO  - - - - - - #######
-data=DataPortal()
+data = DataPortal()
 
-data.load(filename=path_datos+'data_gen.tab', param=(model.gen_barra,model.gen_pmax,model.gen_pmin,model.gen_cvar,model.gen_falla), index=model.GENERADORES)
+data.load(filename=path_datos+'data_gen.tab',
+          param=(model.gen_barra, model.gen_pmax, model.gen_pmin, model.gen_cvar, model.gen_falla),
+          index=model.GENERADORES)
 
 data.load(filename=path_datos+'data_lin.tab',
-          param=(model.linea_fmax,model.linea_barA,model.linea_barB, model.linea_available, model.linea_x), index=model.LINEAS)
+          param=(model.linea_fmax, model.linea_barA, model.linea_barB, model.linea_available, model.linea_x), index=model.LINEAS)
 
-data.load(filename=path_datos+'data_bar.tab', param=(model.demanda), index=model.BARRAS)
-data.load(filename=path_datos+'data_config.tab', param=(model.config_value), index=model.CONFIG)
+data.load(filename=path_datos+'data_bar.tab',
+          param=model.demanda,
+          index=model.BARRAS)
+data.load(filename=path_datos+'data_config.tab',
+          param=model.config_value,
+          index=model.CONFIG)
 
-#model.pprint()
+# model.pprint()
 
 instance = model.create(data)
 
@@ -51,11 +56,11 @@ instance.load(results)
 ####  - - - - - - IMPRIMIENDO SI ES NECESARIO  - - - - - - #######
 
 if instance.config_value['debugging']:
-    stdout_ = sys.stdout #Keep track of the previous value.
+    stdout_ = sys.stdout  #Keep track of the previous value.
     stream = cStringIO.StringIO()
     sys.stdout = stream
-    print instance.pprint() # Here you can do whatever you want, import module1, call test
-    sys.stdout = stdout_ # restore the previous stdout.
+    print instance.pprint()  # Here you can do whatever you want, import module1, call test
+    sys.stdout = stdout_  # restore the previous stdout.
     variable = stream.getvalue()  # This will get the "hello" string inside the variable
 
     output = open(path_resultados+'modelo.txt', 'w')
@@ -65,19 +70,19 @@ if instance.config_value['debugging']:
     output.close()
 
 
-#------R E S U L T A D O S------------------------------------------------------------------------------
-#resultados para GENERADORES---------------------------------------------------------
+# ------R E S U L T A D O S------------------------------------------------------------------------------
+# resultados para GENERADORES---------------------------------------------------------
 ofile  = open(path_resultados+'resultados_generadores.csv', "wb")
 writer = csv.writer(ofile, delimiter=',', quoting=csv.QUOTE_NONE)
-gen=instance.GENERADORES
-scen=instance.SCENARIOS_FALLA_GX
+gen = instance.GENERADORES
+scen = instance.SCENARIOS_FALLA_GX
 
 varpg = getattr(instance, str('GEN_PG'))
 varuc = getattr(instance, str('GEN_UC'))
 varpg_s = getattr(instance, str('GEN_PG_S'))
-tmprow=[]
+tmprow = []
 #header
-header= ['Generador','UC','PG_0']
+header = ['Generador','UC','PG_0']
 for s in scen:
     header.append(str(s))
 writer.writerow(header)
@@ -90,6 +95,6 @@ for g in gen:
     for s in scen:
         tmprow.append(str(varpg_s[g,s].value))
     writer.writerow(tmprow)
-    tmprow=[]
+    tmprow = []
 
-#resultados para LINEAS---------------------------------------------------------
+# resultados para LINEAS---------------------------------------------------------
