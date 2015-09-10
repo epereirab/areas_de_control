9943b87e9d92af0aa57af9b70f977a20e6747b36
Felipe <fgonzalezvenegas@gmail.com>
2015-09-10 13:34:14 -0300

    Adds an output path. Minor changes to improve readability

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-__author__ = 'MPro'
-
 from coopr.pyomo import *
 #from pyomo.environ import *
-from coopr.opt import SolverFactory
-#from pyomo.opt import SolverFactory
-
+#from coopr.opt import SolverFactory
+from pyomo.opt import SolverFactory
 from ReferenceModel import model
 import csv
 import cStringIO
 import sys
 
-path = '/Users/MPro/PycharmProjects/scuc/'
+__author__ = 'MPro'
+
+data_path = '/home/felipe/Documents/CE-AreasControl/areas_de_control/datos/' #Cambiar a un path externo
+output_path = '/home/felipe/Documents/CE-AreasControl/areas_de_control/output/' #Cambiar a un path externo
 data = DataPortal()
 
-data.load(filename=path+'data_gen.tab', param=(model.gen_barra,model.gen_pmax,model.gen_pmin,model.gen_cvar,model.gen_falla), index=model.GENERADORES)
+data.load(filename=data_path+'data_gen.tab',
+          param=(model.gen_barra, model.gen_pmax, model.gen_pmin, model.gen_cvar, model.gen_falla),
+          index=model.GENERADORES)
 
-data.load(filename=path+'data_lin.tab',
-          param=(model.linea_fmax,model.linea_barA,model.linea_barB, model.linea_available, model.linea_x), index=model.LINEAS)
+data.load(filename=data_path+'data_lin.tab',
+          param=(model.linea_fmax, model.linea_barA, model.linea_barB, model.linea_available, model.linea_x),
+          index=model.LINEAS)
 
-data.load(filename=path+'data_bar.tab', param=(model.demanda), index=model.BARRAS)
-data.load(filename=path+'data_config.tab', param=(model.config_value), index=model.CONFIG)
+data.load(filename=data_path+'data_bar.tab',
+          param=model.demanda,
+          index=model.BARRAS)
+data.load(filename=data_path+'data_config.tab',
+          param=model.config_value,
+          index=model.CONFIG)
 
 # model.pprint()
 
@@ -42,16 +49,16 @@ if instance.config_value['debugging']:
     sys.stdout = stdout_ # restore the previous stdout.
     variable = stream.getvalue()  # This will get the "hello" string inside the variable
 
-    output = open(path+'modelo.txt', 'w')
+    output = open(data_path+'modelo.txt', 'w')
     output.write(variable)
-    instance.write(filename=path+'LP.txt',io_options={'symbolic_solver_labels':True})
-    #sys.stdout.write(instance.pprint())
+    instance.write(filename=output_path+'LP.txt',io_options={'symbolic_solver_labels':True})
+    # sys.stdout.write(instance.pprint())
     output.close()
 
 
 # ------R E S U L T A D O S------------------------------------------------------------------------------
 # resultados para GENERADORES---------------------------------------------------------
-ofile = open(path+'resultados_generadores.csv', "wb")
+ofile = open(output_path+'resultados_generadores.csv', "wb")
 writer = csv.writer(ofile, delimiter=',', quoting=csv.QUOTE_NONE)
 gen = instance.GENERADORES
 scen = instance.SCENARIOS_FALLA_GX
