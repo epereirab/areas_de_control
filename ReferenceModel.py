ad349f1a0680a519df6604bffb7753e1679f6f28
Felipe <fgonzalezvenegas@gmail.com>
2015-09-16 16:44:07 -0300

    Agrega reserva como variable. Agrega fallas de lineas como contingencias posibles. Exporta datos de costos del sistema.

--- ReferenceModel.py
+++ ReferenceModel.py
@@ -37,12 +37,15 @@ model.gen_pmin = Param(model.GENERADORES)
 model.gen_barra = Param(model.GENERADORES)
 model.gen_cvar = Param(model.GENERADORES)
 model.gen_falla = Param(model.GENERADORES)
+model.gen_rupmax = Param(model.GENERADORES)
+model.gen_rdnmax = Param(model.GENERADORES)
 
 # LINEAS
 model.linea_fmax = Param(model.LINEAS)
 model.linea_barA = Param(model.LINEAS)
 model.linea_barB = Param(model.LINEAS)
 model.linea_available = Param(model.LINEAS)
+model.linea_falla = Param(model.LINEAS)
 model.linea_x = Param(model.LINEAS)
 
 # BARRAS
@@ -56,12 +59,30 @@ model.config_value = Param(model.CONFIG)
 # SETS FROM PARAMETERS
 ###########################################################################
 
-def falla_scenarios_init(model):
+def falla_scenarios_gx_init(model):
     if model.config_value['scuc']:
         return (g for g in model.GENERADORES if model.gen_falla[g])
     else:
         return []
-model.SCENARIOS_FALLA_GX = Set(initialize=falla_scenarios_init)
+model.SCENARIOS_FALLA_GX = Set(initialize=falla_scenarios_gx_init)
+
+
+def falla_scenarios_tx_init(model):
+    if model.config_value['scuc']:
+        return (l for l in model.LINEAS if model.linea_falla[l])
+    else:
+        return []
+model.SCENARIOS_FALLA_TX = Set(initialize=falla_scenarios_tx_init)
+
+
+def fault_scenarios_init(model):
+    s = []
+    for g in model.SCENARIOS_FALLA_GX:
+        s.append(g)
+    for l in model.SCENARIOS_FALLA_TX:
+        s.append(l)
+    return s
+model.CONTINGENCIAS = Set(initialize=fault_scenarios_init)
 
 ###########################################################################
 # VARIABLES
@@ -78,13 +99,25 @@ model.GEN_PG = Var(model.GENERADORES, within=NonNegativeReals, bounds=bounds_gen
 
 
 # Generacion del generador g, Escenarios de falla
-def bounds_gen_pg_scenario(model, g, sg):
-    if g == sg:
+def bounds_gen_pg_scenario(model, g, s):
+    if g == s:
         return 0, 0
     else:
         return 0, model.gen_pmax[g]
-model.GEN_PG_S = Var(model.GENERADORES, model.SCENARIOS_FALLA_GX,
+model.GEN_PG_S = Var(model.GENERADORES, model.CONTINGENCIAS,
                      within=NonNegativeReals, bounds=bounds_gen_pg_scenario)
+
+
+# Reserva UP del generador g, escenario base
+def bounds_gen_resup(model, g):
+    return 0, model.gen_rupmax[g]
+model.GEN_RESUP = Var(model.GENERADORES, within=NonNegativeReals, bounds=bounds_gen_resup)
+
+
+# Reserva DOWN del generador g, escenario base
+def bounds_gen_resdn(model, g):
+    return 0, model.gen_rdnmax[g]
+model.GEN_RESDN = Var(model.GENERADORES, within=NonNegativeReals, bounds=bounds_gen_resdn)
 
 
 # ENS base
@@ -93,9 +126,10 @@ def bounds_ens(model, b):
 model.ENS = Var(model.BARRAS, within=NonNegativeReals, bounds=bounds_ens)
 
 
-def bounds_ens_scenario(model, b, sg):
+# ENS ESCENARIOS
+def bounds_ens_scenario(model, b, s):
     return 0, model.demanda[b]
-model.ENS_S = Var(model.BARRAS, model.SCENARIOS_FALLA_GX, within=NonNegativeReals, bounds=bounds_ens_scenario)
+model.ENS_S = Var(model.BARRAS, model.CONTINGENCIAS, within=NonNegativeReals, bounds=bounds_ens_scenario)
 
 
 # FLUJO MAXIMO LINEAS
@@ -105,116 +139,138 @@ model.LIN_FLUJO = Var(model.LINEAS, bounds=bounds_fmax)
 
 
 # FLUJO MAXIMO LINEAS SCENARIO
-def bounds_fmax_scenario(model, l, sg):
-    return -model.linea_fmax[l], model.linea_fmax[l]
-model.LIN_FLUJO_S = Var(model.LINEAS, model.SCENARIOS_FALLA_GX, bounds=bounds_fmax_scenario)
+def bounds_fmax_scenario(model, l, s):
+    if l == s:
+        return 0, 0
+    else:
+        return -model.linea_fmax[l], model.linea_fmax[l]
+model.LIN_FLUJO_S = Var(model.LINEAS, model.CONTINGENCIAS, bounds=bounds_fmax_scenario)
 
 
 # ANGULO POR BARRAS
 model.THETA = Var(model.BARRAS, bounds=(-math.pi, math.pi))
 
 # ANGULO POR BARRAS SCENARIO
-model.THETA_S = Var(model.BARRAS, model.SCENARIOS_FALLA_GX, bounds=(-math.pi, math.pi))
+model.THETA_S = Var(model.BARRAS, model.CONTINGENCIAS, bounds=(-math.pi, math.pi))
 
 ###########################################################################
 # CONSTRAINTS
 ###########################################################################
 
 
-##############   C A S O B A S E  #################
+# CONSTRAINT 0: Barra de referencia
+def reference_bar_rule(model):
+    for b in model.BARRAS:
+        if b == model.config_value['default_bar']:
+            return model.THETA[b] == 0
+model.CT_reference_bar = Constraint(rule=reference_bar_rule)
 
-# RESTRICCION 1: abastecimiento de la demanda en cada barra
-def balance_demanda_generacion_rule(model, b):
+
+def reference_bar_rule_contingency(model, s):
+    return sum(model.THETA_S[b, s] for b in model.BARRAS if b == model.config_value['default_bar']) == 0.0
+model.CT_reference_bar_contingency = Constraint(model.CONTINGENCIAS, rule=reference_bar_rule_contingency)
+
+# CONSTRAINT 1: Balance nodal por barra - pre-fault
+def nodal_balance_rule(model, b):
     lside = (sum(model.GEN_PG[g] for g in model.GENERADORES if model.gen_barra[g] == b) +
              sum(model.LIN_FLUJO[l] for l in model.LINEAS if model.linea_barB[l] == b and model.linea_available[l]))
     rside = (model.demanda[b] - model.ENS[b] +
-             sum(model.LIN_FLUJO[l] for l in model.LINEAS if model.linea_barA[l]==b  and model.linea_available[l]))
+             sum(model.LIN_FLUJO[l] for l in model.LINEAS if model.linea_barA[l] == b and model.linea_available[l]))
 
     return lside == rside
 
-model.CT_balance_demanda_generacion = Constraint(model.BARRAS, rule=balance_demanda_generacion_rule)
+model.CT_nodal_balance = Constraint(model.BARRAS, rule=nodal_balance_rule)
 
 
-# RESTRICCION 2 y 3: potencias mínima y máxima
-def p_min_generadores_rule(model, g):
-    return model.GEN_PG[g] >= model.GEN_UC[g] * model.gen_pmin[g]
-
-
-def p_max_generadores_rule(model, g):
-    return model.GEN_PG[g] <= model.GEN_UC[g] * model.gen_pmax[g]
-
-model.CT_potencia_minima = Constraint(model.GENERADORES, rule=p_min_generadores_rule)
-
-model.CT_potencia_maxima = Constraint(model.GENERADORES, rule=p_max_generadores_rule)
-
-
-# RESTRICCION 4: flujo máximo y ángulos por lineas
-def l_max_kcof_rule(model, l):
-    rside = model.LIN_FLUJO[l]
-    lside = 100 * (model.THETA[model.linea_barB[l]] - model.THETA[model.linea_barA[l]]) / model.linea_x[l]
-    return rside == lside
-
-model.CT_lineas_2ley_kcof = Constraint(model.LINEAS, rule=l_max_kcof_rule)
-
-
-##############   ESCENARIOS DE FALLA  #################
-# RESTRICCION 1: abastecimiento de la demanda en cada barra
-def balance_demanda_generacion_scen_rule(model, b, sg):
-    lside = (sum(model.GEN_PG_S[g, sg]
-                 for g in model.GENERADORES if model.gen_barra[g] == b and g != sg) +
-             sum(model.LIN_FLUJO_S[l, sg]
+# CONSTRAINT 1: Balance nodal por barra - post-fault
+def nodal_balance_contingency_rule(model, b, s):
+    lside = (sum(model.GEN_PG_S[g, s]
+                 for g in model.GENERADORES if model.gen_barra[g] == b and g != s) +
+             sum(model.LIN_FLUJO_S[l, s]
                  for l in model.LINEAS if model.linea_barB[l] == b and model.linea_available[l]))
-    rside = (model.demanda[b] - model.ENS_S[b, sg] +
-             sum(model.LIN_FLUJO_S[l, sg]
+    rside = (model.demanda[b] - model.ENS_S[b, s] +
+             sum(model.LIN_FLUJO_S[l, s]
                  for l in model.LINEAS if model.linea_barA[l] == b and model.linea_available[l]))
 
     return lside == rside
 
-model.CT_balance_demanda_generacion_scenario = Constraint(model.BARRAS, model.SCENARIOS_FALLA_GX,
-                                                          rule=balance_demanda_generacion_scen_rule)
+model.CT_nodal_balance_contingency = Constraint(model.BARRAS, model.CONTINGENCIAS,
+                                                rule=nodal_balance_contingency_rule)
 
 
-# RESTRICCION 2 y 3: potencias mínima y máxima
-def p_min_generadores_scen_rule(model, g, sg):
-    if g == sg:
+# CONSTRAINT 2 y 3: Pmin & Pmax - Pre-fault
+def p_min_generators_rule(model, g):
+    return model.GEN_PG[g] - model.GEN_RESDN[g] >= model.GEN_UC[g] * model.gen_pmin[g]
+
+
+def p_max_generators_rule(model, g):
+    return model.GEN_PG[g] + model.GEN_RESUP[g] <= model.GEN_UC[g] * model.gen_pmax[g]
+
+model.CT_min_power = Constraint(model.GENERADORES, rule=p_min_generators_rule)
+
+model.CT_max_power = Constraint(model.GENERADORES, rule=p_max_generators_rule)
+
+
+# CONSTRAINT 2 y 3: Pmin & Pmax - Post-fault
+def p_min_generators_contingency_rule(model, g, s):
+    if g == s:
         return Constraint.Skip
-    return model.GEN_PG_S[g, sg] >= model.GEN_UC[g] * model.gen_pmin[g]
+    return model.GEN_PG_S[g, s] >= model.GEN_PG[g] - model.GEN_RESDN[g]
 
 
-def p_max_generadores_scen_rule(model, g, sg):
-    if g == sg:
+def p_max_generators_contingency_rule(model, g, s):
+    if g == s:
         return Constraint.Skip
-    return model.GEN_PG_S[g, sg] <= model.GEN_UC[g] * model.gen_pmax[g]
+    return model.GEN_PG_S[g, s] <= model.GEN_PG[g] + model.GEN_RESUP[g]
 
-model.CT_potencia_minima_scenario = Constraint(model.GENERADORES, model.SCENARIOS_FALLA_GX,
-                                               rule=p_min_generadores_scen_rule)
-model.CT_potencia_maxima_scenario = Constraint(model.GENERADORES, model.SCENARIOS_FALLA_GX,
-                                               rule=p_max_generadores_scen_rule)
+model.CT_min_power_contingency = Constraint(model.GENERADORES, model.CONTINGENCIAS,
+                                            rule=p_min_generators_contingency_rule)
+model.CT_max_power_contingency = Constraint(model.GENERADORES, model.CONTINGENCIAS,
+                                            rule=p_max_generators_contingency_rule)
 
 
-# RESTRICCION 4: flujo máximo y ángulos por lineas
-def l_max_kcof_scen_rule(model, l, sg):
-    rside = model.LIN_FLUJO_S[l, sg]
-    lside = 100 * (model.THETA_S[model.linea_barB[l], sg] - model.THETA_S[model.linea_barA[l], sg]) / model.linea_x[l]
+# CONSTRAINT 4: DC Flow - pre-fault
+def kirchhoff_rule(model, l):
+    rside = model.LIN_FLUJO[l]
+    lside = 100 * (model.THETA[model.linea_barB[l]] - model.THETA[model.linea_barA[l]]) / model.linea_x[l]
     return rside == lside
 
-model.CT_lineas_2ley_kcof_scenarios = Constraint(model.LINEAS, model.SCENARIOS_FALLA_GX, rule=l_max_kcof_scen_rule)
+model.CT_kirchhoff_2nd_law = Constraint(model.LINEAS, rule=kirchhoff_rule)
+
+
+# CONSTRAINT 4: DC Flow - post-fault
+def kirchhoff_contingency_rule(model, l, s):
+    if l == s:
+        return Constraint.Skip
+    rside = model.LIN_FLUJO_S[l, s]
+    lside = 100 * (model.THETA_S[model.linea_barB[l], s] - model.THETA_S[model.linea_barA[l], s]) / model.linea_x[l]
+    return rside == lside
+
+model.CT_kirchhoff_2nd_law_contingency = Constraint(model.LINEAS, model.CONTINGENCIAS,
+                                                    rule=kirchhoff_contingency_rule)
+
+
+# CONSTRAINT 5: NO ENS in intact system
+# def bah(model, b):
+#     return model.ENS[b] == 0
+#
+# model.CT_noENS = Constraint(model.BARRAS, rule=bah)
 
 
 ###########################################################################
 # FUNCION OBJETIVO
 ###########################################################################
 
-def costo_operacion_rule(model):
+def system_cost_rule(model):
     costo_base = (sum(model.GEN_PG[g] * model.gen_cvar[g] for g in model.GENERADORES) +
                   sum(model.ENS[b] * model.config_value['voll'] for b in model.BARRAS))
 
-    costo_por_scenario = (sum(model.GEN_PG_S[g, sg] * model.gen_cvar[g]
-                              for g in model.GENERADORES for sg in model.SCENARIOS_FALLA_GX) +
-                          sum(model.ENS_S[b, sg] * model.config_value['voll']
-                              for b in model.BARRAS for sg in model.SCENARIOS_FALLA_GX))
+    costo_por_scenario = (sum(model.GEN_PG_S[g, s] * model.gen_cvar[g]
+                              for g in model.GENERADORES for s in model.CONTINGENCIAS) +
+                          sum(model.ENS_S[b, s] * model.config_value['voll']
+                              for b in model.BARRAS for s in model.CONTINGENCIAS))
 
     return costo_base + costo_por_scenario
 
 
-model.Objective_rule = Objective(rule=costo_operacion_rule, sense=minimize)
+model.Objective_rule = Objective(rule=system_cost_rule, sense=minimize)
--- main.py
+++ main.py
@@ -29,11 +29,13 @@ for line in config_rutas:
 data = DataPortal()
 
 data.load(filename=path_datos+'data_gen.tab',
-          param=(model.gen_barra, model.gen_pmax, model.gen_pmin, model.gen_cvar, model.gen_falla),
+          param=(model.gen_barra, model.gen_pmax, model.gen_pmin, model.gen_cvar, model.gen_falla,
+                 model.gen_rupmax, model.gen_rdnmax),
           index=model.GENERADORES)
 
 data.load(filename=path_datos+'data_lin.tab',
-          param=(model.linea_fmax, model.linea_barA, model.linea_barB, model.linea_available, model.linea_x),
+          param=(model.linea_fmax, model.linea_barA, model.linea_barB, model.linea_available, model.linea_x,
+                 model.linea_falla),
           index=model.LINEAS)
 
 data.load(filename=path_datos+'data_bar.tab',
@@ -73,7 +75,7 @@ if instance.config_value['debugging']:
 
 # ------R E S U L T A D O S------------------------------------------------------------------------------
 gen = instance.GENERADORES
-scen = instance.SCENARIOS_FALLA_GX
+scen = instance.CONTINGENCIAS
 lin = instance.LINEAS
 bar = instance.BARRAS
 
@@ -81,24 +83,29 @@ bar = instance.BARRAS
 ofile = open(path_resultados + 'resultados_generadores.csv', "wb")
 writer = csv.writer(ofile, delimiter=',', quoting=csv.QUOTE_NONE)
 
-varpg = getattr(instance, str('GEN_PG'))
-varuc = getattr(instance, str('GEN_UC'))
-varpg_s = getattr(instance, str('GEN_PG_S'))
+# varpg = getattr(instance, str('GEN_PG'))
+# varuc = getattr(instance, str('GEN_UC'))
+# varpg_s = getattr(instance, str('GEN_PG_S'))
 
 tmprow = []
 # header
-header = ['Generador', 'UC', 'PG_0']
+header = ['Generador', 'UC', 'PG_0', 'RES_UP', 'RES_DN']
 for s in scen:
     header.append(str(s))
 writer.writerow(header)
 
 for g in gen:
     tmprow.append(g)
-    tmprow.append(str(varuc[g].value))
-    tmprow.append(str(varpg[g].value))
-
+    #tmprow.append(str(varuc[g].value))
+    #tmprow.append(str(varpg[g].value))
+    tmprow.append(instance.GEN_UC[g].value)
+    tmprow.append(instance.GEN_PG[g].value)
+    tmprow.append(instance.GEN_RESUP[g].value)
+    tmprow.append(instance.GEN_RESDN[g].value)
     for s in scen:
-        tmprow.append(str(varpg_s[g, s].value))
+        tmprow.append(instance.GEN_PG_S[g, s].value)
+        # tmprow.append(str(varpg_s[g, s].value))
+
     writer.writerow(tmprow)
     tmprow = []
 ofile.close()
@@ -147,3 +154,67 @@ for b in bar:
     tmprow = []
 
 ofile.close()
+# Costo total del sistema
+
+
+
+def costo_base():
+    return (sum(instance.GEN_PG[g].value * instance.gen_cvar[g] for g in instance.GENERADORES) +
+            sum(instance.ENS[b].value * instance.config_value['voll'] for b in instance.BARRAS))
+
+def costo_escenario(s):
+    return (sum(instance.GEN_PG_S[g, s].value * instance.gen_cvar[g] for g in instance.GENERADORES) +
+            sum(instance.ENS_S[b, s].value * instance.config_value['voll'] for b in instance.BARRAS))
+
+def costo_ENS():
+    return sum(instance.ENS[b].value * instance.config_value['voll'] for b in instance.BARRAS)
+
+def costo_ENS_escenario(s):
+    return sum(instance.ENS_S[b, s].value * instance.config_value['voll'] for b in instance.BARRAS)
+
+def costo_op():
+    return sum(instance.GEN_PG[g].value * instance.gen_cvar[g] for g in instance.GENERADORES)
+
+def costo_op_escenario(s):
+    return sum(instance.GEN_PG_S[g, s].value * instance.gen_cvar[g] for g in instance.GENERADORES)
+
+
+
+# Resultados del Sistema (ENS)---------------------------------------------------------
+
+ofile = open(path_resultados + 'resultados_system.csv', "wb")
+writer = csv.writer(ofile, delimiter=',', quoting=csv.QUOTE_NONE)
+
+tmprow = []
+# header
+header = ['Valor', '0']
+for s in scen:
+    header.append(str(s))
+writer.writerow(header)
+
+tmprow.append('CostoTotal')
+tmprow.append(costo_base())
+for s in scen:
+    tmprow.append(costo_escenario(s))
+writer.writerow(tmprow)
+tmprow = []
+
+tmprow.append('CostoOperacion')
+tmprow.append(costo_op())
+for s in scen:
+    tmprow.append(costo_op_escenario(s))
+writer.writerow(tmprow)
+tmprow = []
+
+tmprow.append('CostoENS')
+tmprow.append(costo_ENS())
+for s in scen:
+    tmprow.append(costo_ENS_escenario(s))
+writer.writerow(tmprow)
+tmprow = []
+
+ofile.close()
+
+
+
+
